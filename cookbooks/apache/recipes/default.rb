9ffd01fb5647b753092aaebcf03dd6415072b0a6
aberry <berryaaron@comcast.net>
2017-12-07 02:27:26 +0000

    fixed brakets

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,9 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+# Install Apache Package
 
 if node ['platform_family'] == "rhel"
-	package = 'http'
+	package = 'httpd'
 elsif node ['platform_family'] == "debian"
 	package = "apache2"
 end
